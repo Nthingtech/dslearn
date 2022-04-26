7336f6461903cd2fa2d6e48b85cb5d38bbee5630
Fernando Lopes dos Santos <store.nthing@gmail.com>
2022-04-25 21:29:22 -0300

    Course and seed data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Pilates', 'https://img.olhardigital.com.br/wp-content/uploads/2017/01/20170113111619-860x450.jpg', 'https://blogcorretor.aocubo.com/wp-content/uploads/2022/01/aaasd-1140x735.jpg' );
