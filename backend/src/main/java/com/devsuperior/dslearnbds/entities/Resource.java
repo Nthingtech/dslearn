b921a1f710a144a465acdb4b3343b711bb97dbc7
Nthing <store.nthing@gmail.com>
2022-06-09 19:59:39 -0300

    Create Class Resource and ResourceType

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+
+public class Resource implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String title;
+    private String description;
+    private Integer position;
+    private String imgUri;
+
+
+}
